283dd54b8c3a131922a63e266a2ae9bff2c63477
Pratit Bangdiwala <pratit.b@media.net>
2023-04-27 22:02:22 +0530

    responsive changes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
+// variables
 const mobMenuBtn = document.querySelector('.mobile_btn')
 const mobMenu = document.querySelector('.mobile_menu')
 const tubeOne = document.querySelector('.tube_one')
 const tubeTwo = document.querySelector('.tube_two')
+const allSections = document.querySelectorAll('section[id]')
 
 
+// event listeners
 mobMenuBtn.addEventListener('click', function(){
-    openClose()
+    openMenu()
     animateBtn()
 })
+mobMenu.addEventListener('click', closeMenu)
+window.addEventListener('scroll', navHighlighter)
 
-function openClose(){
+// functions
+function openMenu(){
     if(!mobMenu.classList.contains('menuActive')){
         mobMenu.classList.add('menuActive')
     }
@@ -25,5 +31,41 @@ function animateBtn(){
     }
     else{
         mobMenuBtn.classList.remove('tubeActive')
+    }
+}
+
+function navHighlighter(){
+    let scrollYAxis = window.scrollY
+    // console.log(scrollYAxis)
+    allSections.forEach(currentSection => {
+        console.log(currentSection, 'current section')
+        const sectionHeight = currentSection.offsetHeight;
+        console.log(sectionHeight, 'section height')
+        const sectionTop = currentSection.offsetTop - 50;
+        console.log(sectionTop, 'section top')
+        sectionId = currentSection.getAttribute("id");
+
+        if(scrollYAxis > sectionTop && scrollY <= sectionTop + sectionHeight ){
+            console.log(sectionId, 'sectionn id')
+            let foundSection = document.querySelector(".nav_links a[href*=" + sectionId + "]");
+            console.log(foundSection, 'found')
+            foundSection.classList.add('navLinkActive')
+            
+            // console.log(hereSection, 'here section')
+            // hereSectionAdd.classList.add('navLinkActive')
+        }
+        else{
+            let foundSection1 = document.querySelector(".nav_links a[href*=" + sectionId + "]");
+            foundSection1.classList.remove('navLinkActive')
+        }
+    })
+}
+
+function closeMenu(element){
+    if(element.target.classList.contains('mob_link')){
+        mobMenu.classList.remove('menuActive')
+        setTimeout(() => {
+            mobMenuBtn.classList.remove('tubeActive')
+        }, 500);
     }
 }
